61013ff5217d5db35f57e7b8aba2a71869f79ce5
Davi Rodrigues de Souza Machado <davisouzamachado@outlook.com>
2024-10-20 08:51:07 -0300

    Initial commit

---
+++ src/main/java/com/example/desafio_act_digital/DesafioActDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafio_act_digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioActDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioActDigitalApplication.class, args);
+	}
+
+}
